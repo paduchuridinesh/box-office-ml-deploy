9fc6d4340da36d823e96dd9ca667d20472e5f37f
Dinesh Kumar Paduchuri <paduchuridinesh15218@gmail.com>
2025-11-16 16:52:15 +0530

    Update manifest.pp

--- manifest.pp
+++ manifest.pp
@@ -3,11 +3,12 @@ package { 'python3-pip':
 }
 
 exec { 'install_requirements':
-  command => '/usr/bin/pip3 install -r /home/ubuntu/mlapp/requirements.txt',
+  command => '/usr/bin/pip3 install -r /home/ubuntu/mlapp/requirements.txt --break-system-packages',
   refreshonly => false,
 }
 
 exec { 'start_app':
-  command => 'nohup python3 /home/ubuntu/mlapp/app.py > /home/ubuntu/app.log 2>&1 &',
-  unless  => 'pgrep -f app.py',
+  command => 'nohup /usr/bin/python3 /home/ubuntu/mlapp/app.py > /home/ubuntu/app.log 2>&1 &',
+  unless  => '/usr/bin/pgrep -f app.py',
+  path    => ['/usr/bin', '/bin', '/usr/sbin', '/sbin'],
 }
